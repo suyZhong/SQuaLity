9b4be72646a240359a107ea093e0c9d2ee41c231
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2022-10-30 21:48:07 +0800

    Do not complain if the demo directories already exist

--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir logs
-mkdir output
+mkdir logs -p
+mkdir output -p
 python3 main.py --dbms duckdb -t demo/sqlite_tests-index-orderby_nosort-10-slt_good_23.test -f output/temp.db --log DEBUG
