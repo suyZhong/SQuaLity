55b0c457e0da02aa5ee070e68d343886873c4e49
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2022-10-30 23:20:24 +0800

    Make the main.py file executable

--- demo.sh
+++ demo.sh
@@ -2,4 +2,4 @@
 
 mkdir logs -p
 mkdir output -p
-python3 main.py --dbms duckdb -t demo/sqlite_tests-index-orderby_nosort-10-slt_good_23.test -f output/temp.db --log DEBUG
+./main.py --dbms duckdb -t demo/sqlite_tests-index-orderby_nosort-10-slt_good_23.test -f output/temp.db --log DEBUG
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 from src import testparser
 from src import testrunner
