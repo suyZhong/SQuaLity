9feba1c1f3f35f704a98e2a10a9cb814d52a7371
suyang zhong <zsy43847@163.com>
2022-11-07 13:53:22 +0800

    Refactor extract_testcase

--- extract_testcase.py
+++ extract_testcase.py
@@ -3,23 +3,34 @@ from src import testparser
 from src.testcollector import TestcaseCollector, find_local_tests
 
 
-if __name__ == "__main__":
-    # Extract SQL Logic Test (SQLite testcase)
-    slt_parser = testparser.SLTParser()
-    slt_collector = TestcaseCollector()
-    test_files = find_local_tests("sqlite")
-    os.system("mkdir data/sqlite -p")  # TODO make it be a parameter
+def extract(dbms_name:str, parser:testparser.Parser, compression:bool = True):
+    collector = TestcaseCollector()
+    test_files = find_local_tests(dbms_name)
+    os.system("mkdir data/{} -p".format(dbms_name))
 
     for i, test_file in enumerate(test_files):
         # parse the file to get records
-        slt_parser.get_file_name(test_file)
-        slt_parser.get_file_content()
-        slt_parser.parse_file()
+        print(test_file)
+        parser.get_file_name(test_file)
+        parser.get_file_content()
+        parser.parse_file()
 
         # save the records to a csv
         testcase_name = "-".join(test_file.removeprefix(
-            "sqlite_tests/").replace(".test", ".csv").split('/'))
-        records = slt_parser.get_records()
-        slt_collector.init_testcase_schema('sqlite', testcase_name)
-        slt_collector.save_records(records)
-        slt_collector.dump_to_csv()
+            "{}_tests/".format(dbms_name)).replace(".test", ".csv").split('/'))
+        records = parser.get_records()
+        # print(len(records))
+        
+        collector.init_testcase_schema(dbms_name, testcase_name, compression)
+        collector.save_records(records)
+        collector.dump_to_csv()
+        # exit(0)
+
+if __name__ == "__main__":
+    # Extract SQL Logic Test (SQLite testcase)
+    slt_parser = testparser.SLTParser()
+    extract('sqlite', slt_parser)
+
+    # Extract DuckDB Test
+    ddt_parser = testparser.DTParser()
+    extract('duckdb', ddt_parser, compression=False)
