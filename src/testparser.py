1fd451e76ce5685a94a3132b7c4b2a6031022393
suyang zhong <zsy43847@163.com>
2022-11-07 15:13:34 +0800

    Refactor the query part of testparser

--- src/testparser.py
+++ src/testparser.py
@@ -1,7 +1,16 @@
+import re
 from copy import copy
 import pandas as pd
 from .utils import *
 from typing import List
+
+
+def strip_comment_lines(code: str):
+    return re.sub(r'(?m)^ *#.*\n?', '', code)
+
+
+def strip_comment_suffix(code: str):
+    return re.sub(r'--.*', '', code)
 
 
 class Parser:
@@ -68,9 +77,48 @@ class SLTParser(Parser):
 
     def __init__(self, filename='') -> None:
         super().__init__(filename)
-        self.scripts = ""
+        self.scripts = []
         self.dbms_set = DBMS_Set
 
+    def get_query(self, tokens, lines):
+        # A query record begins with a line of the following form:
+        #       query <type-string> <sort-mode> <label>
+        data_type = ''
+        sort_mode = SortType.NO_SORT
+        label = ''
+        # pop out 'query'
+        tokens.pop(0)
+        # pop out <type-string>
+        if tokens:
+            data_type = tokens.pop(0)
+        # pop out <sort-mode>
+        if tokens:
+            sort_token = tokens.pop(0)
+            try:
+                sort_mode = self.sort_mode_dict[sort_token]
+            except KeyError:
+                print("sort mode %s not implemented, change to nosort" %
+                      sort_token)
+        if tokens:
+            label = tokens.pop(0)
+        # The SQL for the query is found on second an subsequent lines
+        # of the record up to first line of the form "----" or until the
+        # end of the record. Lines following the "----" are expected results of the query,
+        # one value per line. If the "----" and/or the results are omitted, then the query
+        # is expected to return an empty set.
+        ind = len(lines)
+        for i, line in enumerate(lines):
+            if line == '----':
+                ind = i
+                break
+        sql = '\n'.join(lines[1:ind])
+        result = ""
+        if ind != len(lines):
+            result = '\n'.join(lines[ind + 1:])
+        record = Query(sql=sql, result=result, data_type=data_type,
+                       sort=sort_mode, label=label, id=self.record_id)
+        return record
+    
     def _parse_script_lines(self, lines: list):
         # Now the first line are exact command
         line_num = len(lines)
@@ -89,44 +137,7 @@ class SLTParser(Parser):
                 status), status=status, id=self.record_id)
             self.record_id += 1
         elif record_type == 'query':
-            # A query record begins with a line of the following form:
-            #       query <type-string> <sort-mode> <label>
-            data_type = ''
-            sort_mode = SortType.NO_SORT
-            label = ''
-            # pop out 'query'
-            tokens.pop(0)
-            # pop out <type-string>
-            if tokens:
-                data_type = tokens.pop(0)
-            # pop out <sort-mode>
-            if tokens:
-                sort_token = tokens.pop(0)
-                try:
-                    sort_mode = self.sort_mode_dict[sort_token]
-                except KeyError:
-                    print("sort mode %s not implemented, change to nosort" %
-                          sort_token)
-            if tokens:
-                label = tokens.pop(0)
-
-            # The SQL for the query is found on second an subsequent lines
-            # of the record up to first line of the form "----" or until the
-            # end of the record. Lines following the "----" are expected results of the query,
-            # one value per line. If the "----" and/or the results are omitted, then the query
-            # is expected to return an empty set.
-            ind = line_num
-            for i, line in enumerate(lines):
-                if line == '----':
-                    ind = i
-                    break
-            sql = '\n'.join(lines[1:ind])
-            result = ""
-            if ind != line_num:
-                result = '\n'.join(lines[ind + 1:])
-
-            record = Query(sql=sql, result=result, data_type=data_type,
-                           sort=sort_mode, label=label, id=self.record_id)
+            record = self.get_query(tokens=tokens, lines=lines)
             self.record_id += 1
 
         elif record_type == 'skipif':
@@ -157,7 +168,7 @@ class SLTParser(Parser):
             self.record_id += 1
         else:
             record = self.testfile_dialect_handler(
-                lines=lines, record_type=record_type)
+                lines=lines, record_type=record_type, id=self.record_id)
             if record:
                 return record
             logging.warning("This script has not implement: %s", lines)
@@ -177,6 +188,8 @@ class SLTParser(Parser):
     # Each record is separated from its neighbors by one or more blank line.
 
     def parse_file(self):
+        """ parse the file by double \\n into a list\n. Then call parse_script() 
+        """
         self.scripts = [script.strip()
                         for script in self.test_content.strip().split('\n\n') if script != '']
         # print(self.scripts)
@@ -200,10 +213,6 @@ class SLTParser(Parser):
             print('result:', rec.result)
             print('support db:', rec.db)
             print('status', rec.status)
-            # For query
-            # print('data_type: ',rec.data_type)
-            # print('label:', rec.label)
-            # print('sort:',rec.sort)
 
 
 class MYTParser(Parser):
