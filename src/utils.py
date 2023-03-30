f17fc43c40be84dd6f84f7529af91186c52af003
suyang <zsy43847@163.com>
2023-03-30 15:18:15 +0800

    Add some bug reason

--- src/utils.py
+++ src/utils.py
@@ -112,7 +112,10 @@ OUTPUT_PATH = {
 POSTGRESQL_FILTER = {
     'REGRESS': lambda x: re.search('regresslib', str(x['SQL'])) is not None,
     'SHOWTABLE': lambda x: re.match(r'\\d\+', str(x['SQL'])) is not None,
+    'NOEXIST': lambda x: re.match(r'ERROR: .* does not exist', str(x['ERROR_MSG'])) is not None,
+    'ALREADYEXIST': lambda x: re.match(r'ERROR: .* already exists', str(x['ERROR_MSG'])) is not None,
     'LARGEOBJ': lambda x: x['TESTFILE_PATH'] in ['postgresql_tests/regress/sql/largeobject.sql', ],
+    'COLLATION': lambda x: x['TESTFILE_PATH'] in ['postgresql_tests/regress/sql/char.sql', 'postgresql_tests/regress/sql/varchar.sql'],
 }
 
 class Record:
