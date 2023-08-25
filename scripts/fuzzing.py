0bf2b7a547fbe37e1ff0ef87460a60bd03114153
suyuz <54768211+suyZhong@users.noreply.github.com>
2023-08-25 16:00:40 +0800

    Try fuzzing (#24)
    * Filter expected-fail statements
    
    * Update requirement (#23)
    
    * Update requirement
    
    * Update duckdb version
    
    * Add naive implementation for MySQL and PostgreSQL
    
    * Handle crashes more elegant
    
    * Support MySQL SimpleFuzzer
    
    * Add direct execution
    
    * Remind there is a bug

--- scripts/fuzzing.py
+++ scripts/fuzzing.py
@@ -16,18 +16,25 @@ if __name__ == "__main__":
     arg_parser.add_argument('--log', type=str, default='INFO', help='logging level')
     arg_parser.add_argument('--corpus', type=str, default='output/psql_results_dependency_without_error.csv', help='corpus path')
     arg_parser.add_argument('-d', '--dbms', type=str, default='sqlite', help='DBMS to fuzz')
+    arg_parser.add_argument('-m', '--mode', type=str, default='mutation', help='fuzzing mode')
     arg_parser.parse_args()
     args = arg_parser.parse_args()
     
     # get current time
     time_string = time.strftime("%Y-%m-%d-%H-%M", time.localtime())
+    if os.path.exists('logs/fuzzing') == False:
+        os.makedirs('logs/fuzzing')
     logging.basicConfig(level=getattr(logging, args.log.upper(
-    )), format='%(asctime)s - %(levelname)s - %(message)s', filemode='w', filename=f'logs/fuzzing-{args.dbms}-{time_string}.log')
+    )), format='%(asctime)s - %(levelname)s - %(message)s', filemode='w', filename=f'logs/fuzzing/{args.dbms}-{time_string}.log')
     # logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', filemode='w', filename='logs/debug.log')
     if args.dbms == 'sqlite':
-        fuzzer = fuzzer.SQLiteSimpleFuzzer(seed=args.seed)
+        fuzzer = fuzzer.SQLiteSimpleFuzzer(seed=args.seed, mode = args.mode)
     elif args.dbms == 'duckdb':
-        fuzzer = fuzzer.DuckDBSimpleFuzzer(seed=args.seed)
+        fuzzer = fuzzer.DuckDBSimpleFuzzer(seed=args.seed, mode = args.mode)
+    elif args.dbms == 'psql':
+        fuzzer = fuzzer.PostgreSQLSimpleFuzzer(seed=args.seed, mode = args.mode)
+    elif args.dbms == 'mysql':
+        fuzzer = fuzzer.MySQLSimpleFuzzer(seed=args.seed, mode = args.mode)
     else:
         assert False, 'DBMS not supported'
     fuzzer.fuzz(args.corpus)
--- src/fuzzer.py
+++ src/fuzzer.py
@@ -2,12 +2,14 @@ import pandas as pd
 import logging
 import subprocess
 import re
+import os
 import random
 import signal
 import time
 from tqdm import tqdm
 import string
 from .config import CONFIG
+from .utils import TestCaseColumns
 
 # MAXINT in SQLite is 9223372036854775807
 # from data import filters
@@ -16,7 +18,7 @@ from .config import CONFIG
 class Fuzzer():
     MAX_TIME_PER_RUN = 5
 
-    def __init__(self, seed: int) -> None:
+    def __init__(self, seed: int, mode) -> None:
         self.test_cases: pd.DataFrame = None
         self.sql_list = []
         self.input = ""
@@ -30,10 +32,26 @@ class Fuzzer():
 
     def load(self, path: str) -> pd.DataFrame:
         self.test_cases = pd.read_csv(path)
+        
+    def load_suite(self, path: str) -> pd.DataFrame:
+        all_test = pd.DataFrame()
+        test_files = []
+        g = os.walk(path)
+        for path, _, file_list in g:
+            test_files += [os.path.join(path, file) for file in file_list]
+        for test_file in tqdm(test_files):
+            df = pd.read_csv(test_file)
+            df['TESTFILE_PATH'] = test_file
+            all_test = pd.concat([all_test, df], ignore_index=True)
+        self.test_cases = all_test
+        logging.debug(self.test_cases)
 
     def extract(self, test_case) -> None:
         self.sql_list = self.test_cases['SQL'].tolist()
 
+    def init_db(self, db_name):
+        pass
+    
     def mutate(self) -> None:
         pass
 
@@ -52,10 +70,11 @@ class SimpleFuzzer(Fuzzer):
     OPERATOR_REGEX = re.compile(
         r"(==|!=|<>|<=|>=|=|<|>)")
 
-    def __init__(self, seed) -> None:
-        super().__init__(seed)
+    def __init__(self, seed, mode) -> None:
+        super().__init__(seed, mode)
         self.cmd = []
         self.cli = None
+        self.mode = mode
         self.iter_times = tqdm(range(100)) if logging.getLogger(
         ).getEffectiveLevel() == logging.DEBUG else range(3000)
 
@@ -108,9 +127,23 @@ class SimpleFuzzer(Fuzzer):
     def operator_mutator(self, sql: str) -> str:
         return re.sub(self.OPERATOR_REGEX, self._get_random_op, sql)
 
-    def mutate(self, iter=False) -> None:
+    def filter(self, df) -> None:
+        return df
+    
+    def extract(self, test_case) -> None:
+        logging.info(f"Extracting test case {test_case}")
+        df: pd.DataFrame = self.test_cases
+        df = df[df['TESTFILE_PATH'] == test_case]
+        df = self.filter(df)
+        self.sql_list = [str(sql).removesuffix(
+            ';') + ';' for sql in df['SQL'].tolist()]
+        logging.info(f"Length of sql list: {len(self.sql_list)}")
+    
+    def mutate(self, iter=False, non = False) -> None:
+        if non:
+            self.input = "\n".join(self.sql_list)
+            return
         # replace every number in the sql with a random new number
-
         if iter:
             self.sql_list = [self.constant_mutator(
                 str(sql), random.choice(self.FUZZING_TAG)) for sql in self.sql_list]
@@ -121,7 +154,13 @@ class SimpleFuzzer(Fuzzer):
 
         self.input = self.operator_mutator(self.input)
 
-    def run(self) -> None:
+    def crash_signal(self, err):
+        logging.debug("err: " + err)
+        return self.cli.returncode < 0
+
+    def run(self) -> int:
+        status = True
+        logging.debug(self.cmd)
         self.cli = subprocess.Popen(self.cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE, encoding='utf-8', universal_newlines=True, bufsize=2 << 15)
         out, err = self.cli.communicate(input=self.input)
@@ -129,18 +168,27 @@ class SimpleFuzzer(Fuzzer):
         logging.debug(self.input)
         logging.debug("output: ")
         logging.debug(out)
-        if self.cli.returncode < 0:
+        logging.debug("return code:" + str(self.cli.returncode))
+        logging.debug("error:")
+        logging.debug(err)
+        if self.crash_signal(err):
+            logging.warning(f"return code: {self.cli.returncode}")
             logging.warning(f"err: {err}")
             logging.warning(self.input)
+            status = False
         self.cli.terminate()
+        return status
 
     def fuzz(self, path: str) -> None:
         random.seed(self.seed)
-        self.load(path)
+        if os.path.isdir(path):
+            self.load_suite(path)
+        else:
+            self.load(path)
         self.setup_summary()
         # print time
         # print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-
+        
         test_cases = list(self.test_cases['TESTFILE_PATH'].unique())
         if logging.getLogger().level == logging.DEBUG:
             random.shuffle(test_cases)
@@ -149,11 +197,12 @@ class SimpleFuzzer(Fuzzer):
             self.extract(test_case)
 
             for i in self.iter_times:
-                self.mutate()
+                self.init_db('fuzzing')
+                self.mutate(non = (self.mode == "simple"))
                 signal.signal(signal.SIGALRM, self.timeout_handler)
                 signal.alarm(self.MAX_TIME_PER_RUN)
                 try:
-                    self.run()
+                    status = self.run()
                 except TimeoutError:
                     logging.warning(f"Timeout for test case {test_case}")
                     logging.info(self.input)
@@ -161,6 +210,11 @@ class SimpleFuzzer(Fuzzer):
                     break
                 else:
                     signal.alarm(0)
+                if status == False:
+                    logging.warning(f"Find a bug from test case {test_case}")
+                    break
+                if self.mode == "simple":
+                    break
             if logging.getLogger().level == logging.DEBUG:
                 exit(0)
 
@@ -171,22 +225,14 @@ class SQLiteSimpleFuzzer(SimpleFuzzer):
     SEG_POOL = [(-1, 1), (0, 1), (1, 14), (-9223372036854775807, -
                                            9223372036854775806), (9223372036854775806, 9223372036854775807), (-0.5, 0.5)]
 
-    def __init__(self, seed=233) -> None:
-        super().__init__(seed)
+    def __init__(self, seed=233, mode='mutation') -> None:
+        super().__init__(seed, mode)
         self.cmd = [CONFIG['sqlite_path']]
 
-    def extract(self, test_case) -> None:
-        logging.info(f"Extracting test case {test_case}")
-        df: pd.DataFrame = self.test_cases
-        df = df[df['TESTFILE_PATH'] == test_case]
-        self.sql_list = [str(sql).removesuffix(
-            ';') + ';' for sql in df['SQL'].tolist()]
-
-        # filter generate_series
-        self.sql_list = [sql for sql in self.sql_list if sql.find(
-            "generate_series") == -1]
-        logging.info(f"Length of sql list: {len(self.sql_list)}")
-
+    def filter(self, df) -> None: 
+        # df = df[(df['SQL']).str.contains("generate_series") == False]
+        df = df[df['STATUS'] == True]
+        return df
 
 class DuckDBSimpleFuzzer(SimpleFuzzer):
     INT_POOL = [-1, 0, 1, 9223372036854775807, -
@@ -194,13 +240,40 @@ class DuckDBSimpleFuzzer(SimpleFuzzer):
     SEG_POOL = [(-1, 1), (0, 1), (1, 14), (-9223372036854775807, -
                                            9223372036854775806), (9223372036854775806, 9223372036854775807), (-0.5, 0.5)]
 
-    def __init__(self, seed=233) -> None:
-        super().__init__(seed)
+    def __init__(self, seed=233, mode='mutation') -> None:
+        super().__init__(seed, mode)
         self.cmd = [CONFIG['duckdb_path']]
 
-    def extract(self, test_case) -> None:
-        logging.info(f"Extracting test case {test_case}")
-        df: pd.DataFrame = self.test_cases
-        df = df[df['TESTFILE_PATH'] == test_case]
-        self.sql_list = [str(sql).removesuffix(
-            ';') + ';' for sql in df['SQL'].tolist()]
+
+    def filter(self, df) -> None:
+        df = df[(df['SQL']).str.contains("COPY") == False]
+        # df = df[df['STATUS'] == True]
+        return df
+
+class PostgreSQLSimpleFuzzer(SimpleFuzzer):
+    def __init__(self, seed, mode) -> None:
+        super().__init__(seed, mode)
+        self.cmd =  [
+            'psql', f"postgresql://{CONFIG['postgres_user']}:{CONFIG['postgres_password']}@localhost:{CONFIG['postgres_port']}/fuzzing?sslmode=disable", '-X', '-q']
+
+
+class MySQLSimpleFuzzer(SimpleFuzzer):
+    def __init__(self, seed, mode) -> None:
+        super().__init__(seed, mode)
+        self.cmd =  [
+            'mysql', '--force' ,'-u', CONFIG['mysql_user'], '-p' + CONFIG['mysql_password'], '-P', str(CONFIG['mysql_port']), '-h', '127.0.0.1', 'fuzzing']
+        
+    def crash_signal(self, err:str):
+        return err.find("ERROR 200") != -1
+    
+    def init_db(self, db_name):
+        init_mysql_script = f"""DROP DATABASE IF EXISTS {db_name}; CREATE DATABASE {db_name}; USE {db_name};"""
+        # self.cmd += [db_name]
+        cmd =  self.cmd[:-1] + ["-e", f"{init_mysql_script}"]
+        logging.debug(f"init db {db_name}")
+        logging.debug(cmd)
+        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if result.stderr.decode().find("ERROR") != -1:
+            logging.error(f"err: {result.stderr.decode()}")
+            # logging.error(self.input)
+            exit(0)
